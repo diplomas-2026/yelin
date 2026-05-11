c8ada20bd7c93f8a8666a9e99bcbc50fa6f5539e
DanBel <dan.bel.89@bk.ru>
2026-05-11 04:25:38 +0400

    update

---
+++ yelin-api/src/main/java/com/github/danbel/yelinapi/YelinApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.danbel.yelinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YelinApiApplication.class, args);
+    }
+
+}
