e0cd53eafc8374199e8a6ebd8abd53308273d828
DanBel <dan.bel.89@bk.ru>
2026-05-11 06:52:25 +0400

    update 2026-05-11 06:52:08

--- yelin-api/src/main/java/com/github/danbel/yelinapi/config/WebConfig.java
+++ yelin-api/src/main/java/com/github/danbel/yelinapi/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
